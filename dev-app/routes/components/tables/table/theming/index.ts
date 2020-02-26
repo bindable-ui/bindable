ca0e230ee4a9526fa6442e65a21a298bf030a21b
Luke Larsen <luke@lukelarsen.com>
2020-02-26 13:41:00 -0700

    WIP tables theming

---
+++ dev-app/routes/components/tables/table/theming/index.ts
@@ -0,0 +1,187 @@
+/*
+Copyright 2020, Verizon Media
+Licensed under the terms of the MIT license. See the LICENSE file in the project root for license terms.
+*/
+
+export class TableThemeProperties {
+    public tableThemeCols = [
+        {
+            _class: 'monospaced',
+            colHeadName: 'name',
+            colHeadValue: 'Name',
+        },
+        {
+            _class: 'monospaced',
+            colClass: 't450',
+            colHeadName: 'default',
+            colHeadValue: 'Default',
+        },
+    ];
+
+    public tableThemeProperties = [
+        {
+            default: '',
+            name: '--table-border',
+        },
+        {
+            default: '',
+            name: '--table-font-size',
+        },
+        {
+            default: '',
+            name: '--table-td-padding',
+        },
+        {
+            default: '',
+            name: '--table-td-border',
+        },
+        {
+            default: '',
+            name: '--table-td-color',
+        },
+        {
+            default: '',
+            name: '--table-th-background',
+        },
+        {
+            default: '',
+            name: '--table-th-color',
+        },
+        {
+            default: '',
+            name: '--table-striped-background',
+        },
+        {
+            default: '',
+            name: '--table-hover-background',
+        },
+        {
+            default: '',
+            name: '--table-active-background',
+        },
+        {
+            default: '',
+            name: '--table-row-bar-healthy',
+        },
+        {
+            default: '',
+            name: '--table-row-bar-info',
+        },
+        {
+            default: '',
+            name: '--table-row-bar-warning',
+        },
+        {
+            default: '',
+            name: '--table-row-bar-critical',
+        },
+        {
+            default: '',
+            name: '--table-row-bar-neutral',
+        },
+        {
+            default: '',
+            name: '--table-text-input-cell-padding',
+        },
+        {
+            default: '',
+            name: '--table-drag-background-hover',
+        },
+        {
+            default: '',
+            name: '--table-drag-height',
+        },
+        {
+            default: '',
+            name: '--table-drag-padding-top',
+        },
+        {
+            default: '',
+            name: '--table-drag-padding-right',
+        },
+        {
+            default: '',
+            name: '--table-drag-padding-left',
+        },
+        {
+            default: '',
+            name: '--table-drag-check-height',
+        },
+        {
+            default: '',
+            name: '--table-drag-check-padding-top',
+        },
+        {
+            default: '',
+            name: '--table-background-healthy: var(--c_secondaryDark)',
+        },
+        {
+            default: '',
+            name: '--table-color-healthy',
+        },
+        {
+            default: '',
+            name: '--table-background-warning',
+        },
+        {
+            default: '',
+            name: '--table-color-warning',
+        },
+        {
+            default: '',
+            name: '--table-background-critical',
+        },
+        {
+            default: '',
+            name: '--table-color-critical',
+        },
+        {
+            default: '--table-background-info',
+            name: '',
+        },
+        {
+            default: '',
+            name: '--table-color-info',
+        },
+        {
+            default: '',
+            name: '--table-background-processing',
+        },
+        {
+            default: '',
+            name: '--table-color-processing',
+        },
+        {
+            default: '',
+            name: '--table-sort-padding-right',
+        },
+        {
+            default: '',
+            name: '--table-sort-icon-position',
+        },
+        {
+            default: '',
+            name: '--table-fixed-header-sort-icon-position',
+        },
+        {
+            default: '',
+            name: '--table-sort-icon-none',
+        },
+        {
+            default: '',
+            name: '--table-sort-icon-desc',
+        },
+        {
+            default: '',
+            name: '--table-sort-icon-asc',
+        },
+        {
+            default: '',
+            name: '--table-mobile-label-color',
+        },
+        {
+            default: '',
+            name: '--table-mobile-label-font-family',
+        },
+    ];
+}
