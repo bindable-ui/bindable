a848c7443b42c5d9ad3fa2c33ec98d7261e4d7df
Luke Larsen <luke@lukelarsen.com>
2020-03-02 19:35:12 -0700

    add theming to c-tile

---
+++ dev-app/routes/components/tile/theming/index.ts
@@ -0,0 +1,99 @@
+/*
+Copyright 2020, Verizon Media
+Licensed under the terms of the MIT license. See the LICENSE file in the project root for license terms.
+*/
+
+export class TileThemeProperties {
+    public tileThemeCols = [
+        {
+            _class: 'monospaced',
+            colHeadName: 'name',
+            colHeadValue: 'Name',
+        },
+        {
+            _class: 'monospaced',
+            colClass: 't450',
+            colHeadName: 'default',
+            colHeadValue: 'Default',
+        },
+    ];
+
+    public tileThemeProperties = [
+        {
+            default: 'var(--s-5)',
+            name: '--tile-padding--tile-padding',
+        },
+        {
+            default: 'var(--c_darkGray)',
+            name: '--tile-background',
+        },
+        {
+            default: 'var(--c_charcoal)',
+            name: '--tile-background-hover',
+        },
+        {
+            default: 'var(--c_soot)',
+            name: '--tile-image-background',
+        },
+        {
+            default: 'var(--c_marshmellow)',
+            name: '--tile-h4-color',
+        },
+        {
+            default: 'var(--s-1)',
+            name: '--tile-h4-size',
+        },
+        {
+            default: 'var(--c_gray)',
+            name: '--tile-checked-background',
+        },
+        {
+            default: 'var(--c_marshmellow)',
+            name: '--tile-checked-text-color',
+        },
+        {
+            default: 'var(--c_black)',
+            name: '--tile-drag-background',
+        },
+        {
+            default: 'calc(var(--s-5) * -1)',
+            name: '--tile-tip-top',
+        },
+        {
+            default: 'calc(var(--s-5) * -1)',
+            name: '--tile-tip-right',
+        },
+        {
+            default: 'var(--s-5) var(--s-3)',
+            name: '--tile-tip-padding',
+        },
+        {
+            default: 'var(--c_charcoal)',
+            name: '--tile-tip-background',
+        },
+        {
+            default: 'var(--s-5)',
+            name: '--tile-status-height',
+        },
+        {
+            default: 'var(--c_secondaryMain)',
+            name: '--tile-status-healthy',
+        },
+        {
+            default: 'var(--c_subOneMain)',
+            name: '--tile-status-info',
+        },
+        {
+            default: 'var(--c_subTwoMain)',
+            name: '--tile-status-warning',
+        },
+        {
+            default: 'var(--c_primaryLight)',
+            name: '--tile-status-critical',
+        },
+        {
+            default: '#CCC url(\'../../../global-styles/assets/images/td-processing.gif\')',
+            name: '--tile-status-processing',
+        },
+    ];
+}
