a174fb9f60944fd0d823ec808b7f8c46617b6d74
Luke Larsen <luke@lukelarsen.com>
2020-03-16 13:55:33 -0600

    WIP theme-alt

--- dev-app/main.ts
+++ dev-app/main.ts
@@ -3,8 +3,8 @@ Copyright 2020, Verizon Media
 Licensed under the terms of the MIT license. See the LICENSE file in the project root for license terms.
 */
 
-import {PLATFORM} from 'aurelia-pal';
 import {Aurelia} from 'aurelia-framework';
+import {PLATFORM} from 'aurelia-pal';
 
 import 'ts-polyfill/lib/es2015-collection';
 import 'ts-polyfill/lib/es2016-array-include';
@@ -20,20 +20,21 @@ import 'ts-polyfill/lib/es2020-string';
 import environment from './environment';
 import './stylesheets/dev-app.css';
 import './stylesheets/fonts.css';
+import './stylesheets/theme-alt.css';
 
 export function configure(aurelia: Aurelia) {
-  aurelia.use
-    .standardConfiguration()
-    .plugin(PLATFORM.moduleName('aurelia-dialog'))
+    aurelia.use
+        .standardConfiguration()
+        .plugin(PLATFORM.moduleName('aurelia-dialog'))
     // load the plugin ../src
     // The "resources" is mapped to "../src" in aurelia.json "paths"
-    .feature('resources');
+        .feature('resources');
 
-  aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
+    aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
 
-  if (environment.testing) {
-    aurelia.use.plugin('aurelia-testing');
-  }
+    if (environment.testing) {
+        aurelia.use.plugin('aurelia-testing');
+    }
 
-  aurelia.start().then(() => aurelia.setRoot());
+    aurelia.start().then(() => aurelia.setRoot());
 }
