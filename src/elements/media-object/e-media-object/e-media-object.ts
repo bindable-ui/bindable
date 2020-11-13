3b91c66260da1c0902414eb7a0237e7a1303143e
Luke Larsen <luke@lukelarsen.com>
2020-11-13 10:59:07 -0700

    Add the e-media-object element

---
+++ src/elements/media-object/e-media-object/e-media-object.ts
@@ -0,0 +1,48 @@
+/*
+Copyright 2020, Verizon Media
+Licensed under the terms of the MIT license. See the LICENSE file in the project root for license terms.
+*/
+
+import {bindable} from 'aurelia-framework';
+import {generateRandom} from '../../../helpers/generate-random';
+
+import * as styles from './e-media-object.css.json';
+
+export class EMediaObject {
+    @bindable
+    public background: string = 'var(--c_slate)';
+    @bindable
+    public center: boolean = true;
+    @bindable
+    public mediaWidth: string = 'unset';
+    @bindable
+    public mediaHeight: string = 'unset';
+    @bindable
+    public mediaBackground: string = 'transparent';
+    @bindable
+    public paddingEnds: string = 'var(--s-2)';
+    @bindable
+    public paddingSides: string = 'var(--s-2)';
+    @bindable
+    public pillColor: string = 'var(--c_subOneMain)';
+    @bindable
+    public pillText: string = '';
+    @bindable
+    public leftGutter: string = 'var(--s-3)';
+    @bindable
+    public rightGutter: string = 'var(--s-3)';
+
+    public rightId = generateRandom();
+
+    public styles = styles;
+
+    public attached() {
+        if (typeof this.center !== 'boolean') {
+            this.center = true;
+        }
+
+        if (!$(`#${this.rightId}`).children().length) {
+            $(`#${this.rightId}`).css('display', 'none');
+        }
+    }
+}
