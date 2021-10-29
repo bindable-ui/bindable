0b2e7a87a57e112315e002c256a0481e8db475c7
natrajant <raj.thamizhmani@nbcuni.com>
2021-10-29 10:11:25 -0700

    Update number-to-string.ts

--- src/value-converters/number-to-string.ts
+++ src/value-converters/number-to-string.ts
@@ -1,8 +1,3 @@
-/*
-Copyright 2020, Verizon Media
-Licensed under the terms of the MIT license. See the LICENSE file in the project root for license terms.
-*/
-
 export class NumberToStringValueConverter {
     public toView(value) {
         if (value === null || value === '') return null;
