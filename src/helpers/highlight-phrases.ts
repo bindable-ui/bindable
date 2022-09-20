3d37729d4c721e7dff1f39c5677ec57acafc9153
Dustin Davis <dustin.davis@edgecast.com>
2022-09-20 00:16:24 -0600

    highlight phrase global & case-insensitive

--- src/helpers/highlight-phrases.ts
+++ src/helpers/highlight-phrases.ts
@@ -6,7 +6,8 @@ export const highlightSearchPhrases = (searchPhrases: string[], matchAgainst?: s
         .replace('>', '&gt;');
     if (searchPhrases && searchPhrases.length > 0) {
         searchPhrases.forEach(orig => {
-            title = title.replace(orig, `<span style="background-color: #226684;">${orig}</span>`);
+            const regEx = new RegExp(orig, 'ig');
+            title = title.replace(regEx, `<span style="background-color: #226684;">${orig}</span>`);
         });
     }
     return title;
