307899f737188e6e4f746b11e0b60cd9b5219ead
Dustin Davis <dustin.davis@edgecast.com>
2022-09-20 07:15:06 -0600

    add unit test

---
+++ src/helpers/highlight-phrases.test.ts
@@ -0,0 +1,14 @@
+import {highlightSearchPhrases} from './highlight-phrases';
+
+describe('Highlight Search Phrases Helper', () => {
+    it('should highlight selected search phrases', () => {
+        const searchPhrases = ['dumb', 'and'];
+        const desc = 'Dumb and Dumber';
+        const highlighted = highlightSearchPhrases(searchPhrases, desc);
+        expect(highlighted).toEqual(
+            '<span style="background-color: #226684;">Dumb</span> ' +
+                '<span style="background-color: #226684;">and</span> ' +
+                '<span style="background-color: #226684;">Dumb</span>er',
+        );
+    });
+});
